d20bc4af0ee1c81483a5678ccc50df1c50d22c42
Rafael José <ra.david@hotmail.com>
2022-11-09 23:12:28 +0000

    Addition of RushDown spell for the warrior, Exorcism and Mend spell for the priest.
    Update on the attacks from the priest, warrior and vampire.
    
    More organization in the code.
    
    Correction of the message when the enemy is killed, now negative life no longer appears.
    
    Changed the variable enemy_turnOrder to VampireTurnOrder.

--- main.py
+++ main.py
@@ -12,11 +12,14 @@ command = "" #Initial command for input
 warrior = [32,5,2,5,2] #HP, MP , AP, WP, Init 
 WarriorTurnOrder = 0 #Initial warrior turn order number
 warrior_dmg = 0 #Setting floor for the warrior dmg
+warrior_SpellCost = 5 #Warrior cost to do the spell
 
 #Priest
 priest = [20,25,0,2,6] #HP, MP , AP, WP, Init 
 PriestTurnOrder = 0 #Setting floor for the priest turn order
 priest_dmg = 0 #Setting floor for the priest dmg
+priest_Exorcism_SpellCost = 5 #Exorcism spell cost
+priest_Mend_SpellCost = 3 #Mend spell cost
 
 ###########
 # ENEMIES #
@@ -24,76 +27,111 @@ priest_dmg = 0 #Setting floor for the priest dmg
 #Vampire
 vampire = [15,0,2,2,2] #HP, MP, AP, WP, Init
 vampire_dmg = 0 #Setting floor for the vampire dmg
+VampireTurnOrder = 0 #Initial enemy turn order number
 
 
-###############
-# TURN ORDERS #
-###############
-enemy_turnOrder = 0 #Initial enemy turn order number
-
+###########################
+# COMBAT INITIATIVE PHASE #
+###########################
 def initiative_phase(): #Function for setting the turn order
     #Variables for get in action phase
     global WarriorTurnOrder
     global PriestTurnOrder
-    global enemy_turnOrder
+    global VampireTurnOrder
 
     #Variables to reset the score
     WarriorTurnOrder = 0
     PriestTurnOrder = 0
-    enemy_turnOrder = 0
+    VampireTurnOrder = 0
     
     #Cal the results to who goes first
     WarriorTurnOrder = WarriorTurnOrder + random.randint(0,20) + warrior[4] #Random Value for the Warrior's turn number
     PriestTurnOrder = PriestTurnOrder + random.randint(0,20) + priest[4] #Random Value for the Priest's turn number
-    enemy_turnOrder = enemy_turnOrder + random.randint(0,20) + vampire[4] #Random Value for the Vampire's turn number
+    VampireTurnOrder = VampireTurnOrder + random.randint(0,20) + vampire[4] #Random Value for the Vampire's turn number
     
     #Print the results
     print("\nThe warrior got " + str(WarriorTurnOrder)) 
-    print("The vampire got " + str(enemy_turnOrder))
+    print("The vampire got " + str(VampireTurnOrder))
     print("The priest got " + str(PriestTurnOrder))
 
     #Start Action phase with the numbers cal    
     action_phase()
     
+##########################
+# WARRIOR COMBAT ACTIONS #
+##########################    
 #Attack from the warrior
 def warrior_attack(): #If the vampire got the armor up
-    if vampire [2] > 0: #If the vampire got the armor up, then attack the armor
-        vampire[2] = vampire[2] - random.randrange(warrior[3])
-        if vampire[2] > 0:
+    if vampire[2] > 0: #If the vampire got the armor up, then attack the armor
+        vampire[2] = vampire[2] - random.randrange(warrior[3]) #Enemy's AP less the random number inside WP from the warrior
+        if vampire[2] > 0: 
             print("You damaged the enemy armor! Enemy's current armor :" + str(vampire[2]))
         elif vampire[2] <= 0:
             print("You have destroyed the enemy armor!")
     elif vampire[2] <= 0: #If the vampire got no armor, then attack life
-        vampire[0] = vampire[0] - random.randrange(warrior[3])
-        print("You hit the enemy, enemy current life: " + str(vampire[0]))
-    
-    return vampire
+        vampire[0] = vampire[0] - random.randrange(warrior[3]) #Enemy's HP less the random number inside WP from the warrior
+        if vampire[0] > 0:
+            print("You hit the enemy, enemy current life: " + str(vampire[0]))
 
+#RushDown spell from the warrior  
+def warrior_spell(): #Function for the Warrior Spell
+    d4 = random.randint(1,4) #Four-sided dice
+    SpellEffectValue = -1 * (warrior[3] + d4) #Spell effect and the respective damage
+    vampire[0] = vampire[0] + SpellEffectValue #Damage gave to the enemy
+    warrior[1] = warrior[1] - warrior_SpellCost #Take of MP points after the spell
+
+#########################
+# PRIEST COMBAT ACTIONS #
+#########################
 #Attack from the priest
 def priest_attack():
-    if vampire [2] > 0: #If the vampire got the armor up
-        vampire[2] = vampire[2] - random.randrange(warrior[3])
-        if vampire[2] > 0:
+    if vampire[2] > 0: #If the vampire got the armor up
+        vampire[2] = vampire[2] - random.randrange(priest[3]) #Enemy's AP less the random number inside WP from the priest
+        if vampire[2] > 0: 
             print("You damaged the enemy armor! Enemy's current armor :" + str(vampire[2]))
         elif vampire[2] <= 0:
             print("You have destroyed the enemy armor!")
     elif vampire[2] <= 0: #If the vampire got no armor, attack life
-        vampire[0] = vampire[0] - random.randrange(priest[3])
+        vampire[0] = vampire[0] - random.randrange(priest[3]) #Enemy's HP less the random number inside WP from the priest
         print("You hit the enemy, enemy current life: " + str(vampire[0]))
 
+#Exorcism spell from the priest
+def priest_spell_exorcism():
+    d4 = random.randint(1,4) #Four-sided dice
+    SpellEffectValue = -1 * (d4 * 2) #Spell effect and the respective damage
+    vampire[0] = vampire[0] + SpellEffectValue #Damage gave to the enemy
+    priest[1] = priest[1] - priest_Exorcism_SpellCost #Take of MP points after the spell
+
+#Mend spell from the priest
+def priest_spell_mend():
+    d6 = random.randint(1,6) #Four-sided dice
+    SpellEffectValue = d6 + priest[3] #Spell effect and the respective damage
+    warrior[0] = warrior[0] + SpellEffectValue #Damage gave to the enemy
+    priest[1] = priest[1] - priest_Mend_SpellCost #Take of MP points after the spell
+
+
+########################
+# ENEMY COMBAT ACTIONS #
+########################
 #Attack from the enemy
 def enemy_attack():
     if warrior[2] > 0: #If the warrior got the armor up
         warrior[2] = warrior[2] - random.randrange(vampire[3])
-        print("Your warrior got the armor damaged!")
+        if warrior[2] > 0:
+            print("Your warrior got the armor damaged! Warrior's current armor: " + str(warrior[2]))
+        elif warrior[2] <= 0:
+            print("Your warrior's armor is destroyed!")
     elif warrior[2] <= 0:
         warrior[0] = warrior[0] - random.randrange(vampire[3])
         print("Your warrior got attacked, his current life is: " + str(warrior[0]))
-    elif warrior[0] <= 0: #If the warrior is dead
+    elif warrior[0] <= 0: #If the warrior got killed
         print("Your warrior got killed!") 
-    elif warrior[0] <= 0 and priest[2] > 0:
+    elif priest[2] > 0: 
         priest[2] = priest[2] - random.randrange(vampire[3])
-        print("Your priest got the armor damaged")
+        if priest[2] > 0:
+            print("Your priest got the armor damaged! Priest's current armor: " + str(priest[2]))
+        elif priest[2] <= 0:
+            print("Your warrior's armor is destroyed!")
     
 #######################
 # COMBAT ACTION PHASE #
@@ -104,11 +142,16 @@ def action_phase():
                             "\nChoose your action" + 
                             "\n1. Attack." +
                             "\n2. Magic.\n")
-            if command == "1":
+            if command == "1": #Execute the warrior attack
                 warrior_attack()
-           # elif command == "2":
-                #magic()
-            else:
+            elif command == "2": #Execute the warrior spell
+                if warrior[1] >= warrior_SpellCost: #If the warrior got the MP suf to do the spell
+                    warrior_spell()
+                    print("You have damage your enemy! Enemy's current life: ", vampire[0])
+                else: #If the warrior got no MP suf to do the spell
+                    print("You have no more MP!")
+                    warrior_turn()
+            else: #If the player put's a incorrect input
                 print("Enter a correct action!")
                 warrior_turn()
     def priest_turn():
@@ -116,10 +159,26 @@ def action_phase():
                             "\nChoose your action" + 
                             "\n1. Attack." +
                             "\n2. Magic.\n")
-            if command == "1":
+            if command == "1": #Execute the priest attack
                 warrior_attack()
-            #elif command == "2":
-            #    magic()
+            elif command == "2": #Executes a spell from the priest
+                command = input("\nWhat spell do you wanna use?"+ 
+                                "\n1. Exorcism - Damage the enemy" +
+                                "\n2. Mend - Heal the warrior\n")
+                if command == "1": #Executes the exorcism spell
+                    if priest[1] >= priest_Exorcism_SpellCost:
+                        priest_spell_exorcism()
+                        print("You have damage your enemy! Enemy's current life: ", vampire[0])
+                    else:
+                        print("You have no more MP!")
+                        priest_turn()
+                elif command == "2":
+                    if priest[1] >= priest_Mend_SpellCost:
+                        priest_spell_mend()
+                        print("You healed the warrior! Warrior's current life: ", warrior[0])
+                    else:
+                        print("You have no more Mana Points!")
+                        priest_turn()
             else:
                 print("Enter a correct action!")
                 priest_turn()
@@ -129,31 +188,31 @@ def action_phase():
             enemy_attack()
     
     def first_turn():
-        if (WarriorTurnOrder > enemy_turnOrder) and (WarriorTurnOrder > PriestTurnOrder): #Warrior first
+        if (WarriorTurnOrder > VampireTurnOrder) and (WarriorTurnOrder > PriestTurnOrder): #Warrior first
             warrior_turn()
-        elif (PriestTurnOrder > enemy_turnOrder) and (PriestTurnOrder > WarriorTurnOrder): #Priest first
+        elif (PriestTurnOrder > VampireTurnOrder) and (PriestTurnOrder > WarriorTurnOrder): #Priest first
             priest_turn()
-        elif (enemy_turnOrder > WarriorTurnOrder) and (enemy_turnOrder > PriestTurnOrder): #Enemy goes first
+        elif (VampireTurnOrder > WarriorTurnOrder) and (VampireTurnOrder > PriestTurnOrder): #Enemy goes first
             enemy_turn()
-        elif (WarriorTurnOrder == enemy_turnOrder) or (WarriorTurnOrder == PriestTurnOrder): #In case of draw return to turn order
+        elif (WarriorTurnOrder == VampireTurnOrder) or (WarriorTurnOrder == PriestTurnOrder): #In case of draw return to turn order
             initiative_phase()
-        elif (PriestTurnOrder == enemy_turnOrder) or (PriestTurnOrder == WarriorTurnOrder): #In case of draw return to turn order
+        elif (PriestTurnOrder == VampireTurnOrder) or (PriestTurnOrder == WarriorTurnOrder): #In case of draw return to turn order
             initiative_phase()
-        elif (enemy_turnOrder == WarriorTurnOrder) or (enemy_turnOrder == PriestTurnOrder): #In case of draw return to turn
+        elif (VampireTurnOrder == WarriorTurnOrder) or (VampireTurnOrder == PriestTurnOrder): #In case of draw return to turn
             initiative_phase()
             
     def second_turn():
-        if (WarriorTurnOrder > enemy_turnOrder) and (WarriorTurnOrder < PriestTurnOrder): #Warrior second
+        if (WarriorTurnOrder > VampireTurnOrder) and (WarriorTurnOrder < PriestTurnOrder): #Warrior second
             warrior_turn()
-        elif (PriestTurnOrder > enemy_turnOrder) and (PriestTurnOrder < WarriorTurnOrder): #Priest second
+        elif (PriestTurnOrder > VampireTurnOrder) and (PriestTurnOrder < WarriorTurnOrder): #Priest second
             priest_turn()
-        elif (enemy_turnOrder > WarriorTurnOrder) and (enemy_turnOrder < PriestTurnOrder): #Enemy second
+        elif (VampireTurnOrder > WarriorTurnOrder) and (VampireTurnOrder < PriestTurnOrder): #Enemy second
             enemy_turn()
     
     def third_turn():
-        if (WarriorTurnOrder < enemy_turnOrder) and (WarriorTurnOrder < PriestTurnOrder): #Warrior third
+        if (WarriorTurnOrder < VampireTurnOrder) and (WarriorTurnOrder < PriestTurnOrder): #Warrior third
             warrior_turn()
-        elif (PriestTurnOrder < enemy_turnOrder) and (PriestTurnOrder < WarriorTurnOrder): #Priest third
+        elif (PriestTurnOrder < VampireTurnOrder) and (PriestTurnOrder < WarriorTurnOrder): #Priest third
             priest_turn()
         else: #Enemy third
             enemy_turn()
@@ -170,10 +229,13 @@ def action_phase():
             print("You have died! Better luck next time.")
             break
         elif vampire[0] <= 0: #The enemy die
-            print("You have killed the vampire! Continue your journey.")
+            print("You have killed the vampire! Good job!.")
             break
         else:
             break
 
+####################
+# COMBAT MAIN LOOP #
+####################
 if (warrior[0] > 0 and priest[0] > 0) and vampire[0] > 0: #Check all are alive to start the battle
     initiative_phase()
